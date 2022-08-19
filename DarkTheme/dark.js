4a50fdc292c1152c77dfc7caa4df67cb0be8b529
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-19 16:32:45 +0600

    set Black and white background using javascript

---
+++ DarkTheme/dark.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+function darkBackground(){
+    document.body.style.backgroundColor = "Black";
+}
+function lightBackground(){
+    document.body.style.backgroundColor = "White";
+}
